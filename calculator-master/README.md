5bbefc14d7645ead131f070e36e75d9a8c2271fc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-21 19:24:54 -0300

    feat: caculadora

---
+++ calculator-master/README.md
@@ -0,0 +1,5 @@
+# Calculator HTML - CSS - JAVASCRIPT
+
+## Description
+
+This is a simple calculator made with HTML, CSS and Javascript. It has the basic operations of a calculator, such as addition, subtraction, multiplication and division.
---
+++ calculator-master/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("keydown", (event) => {
+    const key = event.key
+    if (key === "=") {
+        equals()
+    }
+})
+document.addEventListener("keydown", (event) => {
+    const key = event.key
+    if (key === "=") {
+        equals()
+    }
+})
+document.addEventListener("keydown", (event) => {
+    const key = event.key
+    if (key === "=") {
+        equals()
+    }
+})
