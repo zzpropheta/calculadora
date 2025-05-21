a87f608cb90c5748964c0820748cea9a27661b96
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-21 20:05:17 -0300

    refactor: talvez dê errado

--- calculator-master/script.js
+++ calculator-master/script.js
@@ -1,25 +1,81 @@
 const display = document.getElementById('result');
 const buttons = document.querySelectorAll('.btn');
-let currentInput = '';
+let valorAtual = '';
+let num1 = 0;
+let num2 = 0;
+let sinal = '';
+
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         const value = button.textContent;
 
         if (value === 'C') {
-            currentInput = '';
-            display.value = '';
+            display.value = ''
+            valorAtual = '';
+            num1 = 0;
+            num2 = 0;
+            sinal = '';
         } else if (value === '=') {
             try {
-                currentInput = eval(currentInput).toString();
-                display.value = currentInput;
+                if (sinal === '+') {
+                    display.value = num1 + num2;
+                    num1 = display.value;
+                }
+                if (sinal === '-') {
+                    display.value = num1 - num2;
+                    num1 = display.value;
+                }
+                if (sinal === '*') {
+                    display.value = num1 * num2;
+                    num1 = display.value;
+                }
+                if (sinal === '/') {
+                    display.value = num1 / num2;
+                    num1 = display.value;
+                }
+
             } catch {
                 display.value = 'Erro';
-                currentInput = '';
+                valorAtual = '';
             }
+        } else if (value >= 0 && value <= 9) {
+            display.value += value;
+            valorAtual += value;
+        } else if (value === "+") {
+            if (num1 != 0) {
+                num2 = valorAtual;
+                display.value = num1 + num2;
+                num1 = display.value;
+                num2 = 0;
+            } else {
+                num1 = valorAtual;
+                valorAtual = "";
+                sinal = 
+            }
+
+
+        } else if (value === "-") {
+            if (num1) {
+                num2 = valorAtual;
+                display.value = num1 - num2;
+                num1 = display.value;
+            }
+
+
+
+        } else if (value === "X") {
+
+
+
+        } else if (value === "÷") {
+
+
+
+
         } else {
-            currentInput += value;
-            display.value = currentInput;
+            valorAtual += value;
+            display.value = valorAtual;
         }
     });
 });
